539edd653472542091bf76e5ca6757dcccfc5455
Sebastian <sebastian_spanner@hotmail.de>
2013-07-01 19:47:56 +0200

    Some stuff
    - Updated main Window
    - Added Block Interface

---
+++ Quadris/Block.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quadris
+{
+    interface Block
+    {
+        // Properties:
+        int rotation
+        {
+            get;
+            set;
+        }
+
+        public void drawBlock(int[][] gameFieldArray, int centerIndex); //draw block in gameArray
+        public void rotate(); //change rotationstate of block
+        public void checkIfBlockCanBePlaced(int[][] gameFieldArray, int centerIndex); // Check if block can be placed around current centerIndex
+
+    }
+}
