207f00f1321f5c73bdbd0f87e6c40085e3514016
Sebastian <sebastian_spanner@hotmail.de>
2013-07-02 11:31:11 +0200

    Adeed new superclass

---
+++ Quadris/BlockControl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quadris
+{
+    class BlockControl
+    {
+        public void drawBlock(int[][] gameFieldArray, int centerIndexI, int centerIndexJ, int delta1I, int delta1J, int delta2I, int delta2J, int delta3I, int delta3J)
+        {
+
+            gameFieldArray[centerIndexI][centerIndexJ] = 1;
+            gameFieldArray[centerIndexI+delta1I][centerIndexJ+delta1J] = 1;
+            gameFieldArray[centerIndexI + delta2I][centerIndexJ + delta2J] = 1;
+            gameFieldArray[centerIndexI + delta2I][centerIndexJ + delta2J] = 1;
+
+
+        }
+    }
+}
