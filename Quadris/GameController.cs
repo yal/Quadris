d1484953bbc459821e1892c073e5000733bc317e
Sebastian <sebastian_spanner@hotmail.de>
2013-07-07 18:30:38 +0200

    added game and gui controllers

---
+++ Quadris/GameController.cs
@@ -0,0 +1,55 @@
+﻿using Quadris.Blocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quadris
+{
+    class GameController
+    {
+        public Random rnd;
+
+
+        public GameController() {
+            
+            rnd = new Random(); // seed rnd
+        
+        }
+
+        public Block getRandomBlock() {
+
+            int blockType = rnd.Next(7);
+            int rorationState = rnd.Next(4);
+            Block block = null;
+
+            switch (blockType) {
+                case 0:
+                    block = new OBlock();
+                    break;
+                case 1:
+                    block = new IBlock();
+                    break;
+                case 2:
+                    block = new SBlock();
+                    break;
+                case 3:
+                    block = new ZBlock();
+                    break;
+                case 4:
+                    block = new JBlock();
+                    break;
+                case 5:
+                    block = new LBlock();
+                    break;
+                case 6:
+                    block = new TBlock();
+                    break;
+            }
+
+            block.rotate(rorationState);
+            return block;
+        }
+    }
+}
---
+++ Quadris/GuiController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Shapes;
+
+namespace Quadris
+{
+    class GuiController
+    {
+        int placedPieces = 0;
+        public static void resetSelection(Block block, GridView grid) {
+
+            SolidColorBrush brush = new SolidColorBrush(Colors.LightGray);
+
+            for (int i = 0; i < block.getClickedIndexes().GetLength(0); i++)
+            {
+
+                Rectangle rect = grid.Items[block.getClickedIndexes()[i]] as Rectangle;
+                rect.Fill = brush;
+
+            }
+
+            block.resetState();
+
+        }
+
+        internal void setNextBlockImage(Image image, Block block) {
+
+            image.Source = new BitmapImage(new Uri("ms-appx:/Assets/Blocks/" + block.getImageName(), UriKind.Absolute));
+        
+        }
+
+        internal void updateScore(TextBlock label) {
+            placedPieces++;
+            label.Text = "Score : " + (placedPieces * 4) + "%";
+
+        
+        }
+
+    }
+}
